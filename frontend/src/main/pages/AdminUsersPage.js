3cdff246c0d62cf5bf49d4cf3855552473ce636c
Hao <hao_wu@umail.ucsb.edu>
2023-08-24 01:02:22 -0700

    Hao - try out fixing 404

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -5,11 +5,14 @@ import UsersTable from "main/components/Users/UsersTable"
 import { useUsers } from "main/utils/users";
 const AdminUsersPage = () => {
 
-    const { data: users } = useUsers();
+    // const { data: users } = useUsers();
+    const { data: users, isLoading, isError, error } = useUsers();
+    console.log(isLoading);
 
     return (
         <BasicLayout>
             <h2>Users</h2>
+            <div>{isLoading}</div>
             <UsersTable users={users} />
         </BasicLayout>
     );
